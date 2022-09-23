6c9ca78de3e46684c05d24684a2f2903e540abec
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-09-23 09:25:20 +0200

    Add files via upload

---
+++ 11_Char.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _11_Char
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char pismeno = 'A';
+            Console.WriteLine("Písmenko je: " + pismeno);
+
+            // matematika na písmenku (char) 
+            char pismeno2 = (char)(pismeno + 10);
+            // vytvoří větší typ (integer), proto na začátku "přetypováváme" zpět na char
+
+            // char pismeno2 = (char)(pismeno + 'b');
+            Console.WriteLine("Další písmenko je: " + pismeno2);
+            Console.ReadKey();
+        }
+    }
+}
