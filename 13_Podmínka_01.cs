e9b664f16e9df056ecd2a8dd4878b2208c78b5bc
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-10-05 11:55:36 +0200

    Add files via upload

---
+++ 13_Podmínka_01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _13_Podmínka_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej svůj věk");
+            int vek = int.Parse(Console.ReadLine());
+            if (vek < 21) // definuji podmínku
+            // Zde se vyková kód při splní prodmínky
+            {
+                Console.WriteLine("Není ti 21, takže si dáš Colu :)");
+            }
+            else // zde se vykoná kód pri NESPLNĚNÍ podmínky
+            {
+                Console.WriteLine("Alkohol již můžeš, ale s ROZUMEM");
+            }
+            Console.ReadKey();
+        }
+    }
+}
