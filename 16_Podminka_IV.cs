c3c8b38e495652a6542d7b9466aaac66fcae1870
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-10-05 13:08:21 +0200

    Add files via upload

---
+++ 16_Podminka_IV.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hodnota;
+            Console.WriteLine("Zadej cele číslo");
+            int cislo = int.Parse(Console.ReadLine());
+            if (cislo == 0)
+              hodnota = "Nula";
+            else
+            {
+                if (cislo > 0)
+                  hodnota = "Kladné čislo";
+                else
+                 hodnota = "Záporné čislo";
+            }
+
+            Console.WriteLine(hodnota);
+            Console.ReadKey();
+        }
+    }
+}
