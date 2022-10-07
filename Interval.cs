a0c13664bd4eef63926ed717ed052c7173e0e5cb
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-10-07 09:00:30 +0200

    Add files via upload

---
+++ Interval.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _19_Interval
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej číslo v intervalu <-10 ; 10 >");
+            int a = int.Parse(Console.ReadLine());
+
+            if ((a <= -10) || (a >= 10))
+            
+                Console.WriteLine($"Tvé zadané čílos {a} leží v požadovaném intervalu");
+            else
+                Console.WriteLine($"Tvé zadané čílos {a} NEleží v požadovaném intervalu");
+            
+
+            Console.ReadKey();
+        }
+    }
+}
