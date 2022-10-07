a05774116cd288f50ccc9134e516ddc944482f66
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-10-07 08:42:29 +0200

    Add files via upload

---
+++ Negace.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _18_Negace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej číslo 1  nebo 0");
+            int a = int.Parse(Console.ReadLine());
+
+            if ((a == 1) || (a == 0))
+            { if (a == 1)
+                {
+                    a = 0;
+                    Console.WriteLine("Negace zadaného čísla je " + a); 
+                }
+                    
+                else
+                {
+                    a = 1;
+                    Console.WriteLine("Negace zadaného čísla je " + a);
+                }
+            }
+            else
+                Console.WriteLine("Zadal jsi špatné hodnoty");
+            
+            Console.ReadKey();
+        }
+    }
+}
