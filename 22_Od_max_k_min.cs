cae6ffb27d9b223366707f3f1eb7891da5348ebd
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-10-12 13:02:38 +0200

    Add files via upload

---
+++ 22_Od_max_k_min.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _22_Od_max_k_min
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej číslo A");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej číslo B");
+            int b = int.Parse(Console.ReadLine());
+            if ( a > b)
+                {
+                int p = a;
+                a = b;
+                b = p;
+            }
+            Console.WriteLine("Zadej číslo C");
+            int c = int.Parse(Console.ReadLine());
+            if (b > c)
+                {
+                int p = c;
+                c = b;
+                b = p;
+                }
+            if (a > b)
+                {
+                int p = a;
+                a = b;
+                b = p;
+            }
+            
+            Console.WriteLine($"Platí, že {c} < {b} < {a}");
+            Console.ReadKey();
+        }
+    }
+}
