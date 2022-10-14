f5ef0d8b0d69218c972cc2cd5cef63d67567e8aa
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-10-14 08:41:39 +0200

    Add files via upload

---
+++ 23_prvni_velke_pismeno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    internal class První_velké_pismeno
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej nazev školy");
+            string skola = Console.ReadLine();
+            skola = char.ToUpper(skola[0]) + skola.Substring(1);
+            Console.WriteLine(skola);
+            Console.ReadKey();
+
+              
+        }
+    }
+}
