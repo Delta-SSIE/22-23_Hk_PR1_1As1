47dd2560790fdede691d23d28fc84c8f7f79745c
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-10-14 09:29:27 +0200

    Add files via upload

---
+++ 24_Case.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _24_Dny_v_tydnu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej číslo dne v týdnu:");
+            int cislo = int.Parse(Console.ReadLine());
+
+            string den;
+
+            // zde definuji výběr z více možností
+
+            switch (cislo)
+            {
+                case 1:
+                    den = "Pondělí";
+                    break;
+                case 2:
+                    den = "Úterý";
+                    break;
+                case 3:
+                    den = "Středa";
+                    break;
+                case 4:
+                    den = "Čtvrtek";
+                    break;
+                case 5:
+                    den = "Pátek";
+                    break;
+                case 6:
+                    den = "Sobota";
+                    break;
+                case 7:
+                    den = "Neděle";
+                    break;
+                default:
+                    den = "Zadal jsi nějaký divný den !";
+                    break;
+            }
+            Console.WriteLine($"Zadanému číslu {cislo} dne v týdnu říkáme {den}");
+            Console.ReadKey();
+        }
+    }
+}
