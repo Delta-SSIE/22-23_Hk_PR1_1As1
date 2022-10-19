7eb9028a14f8d0e21f324687b4a7119660aa255f
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-10-19 12:32:03 +0200

    Add files via upload

---
+++ 25_Genrator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _25_Genrator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random(); // vytvářím konstruktor třídy Random se jménem generator
+            Console.WriteLine("Náhodně vygeneruji číslo z intervalu <-5, 5 >");
+            int a = generator.Next(-5, 6); // definuji rozsah (od, do), kde do již nepatří do intervalu
+            Console.WriteLine($"Náhodně jsem vygeneroval číslo {a}");
+            Console.ReadKey();
+
+            if (a >= 0)
+                Console.WriteLine($"Vygenerované číslo {a} je kladné");
+            else
+                Console.WriteLine($"Vygenerované číslo {a} je záporné");
+
+            Console.ReadKey();
+        }
+    }
+}
