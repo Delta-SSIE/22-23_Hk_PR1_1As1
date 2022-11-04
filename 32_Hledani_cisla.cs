f51b5d642f1860bb58199ff2b7b12017a8207e21
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-11-04 09:31:40 +0100

    Add files via upload

---
+++ 32_Hledani_cisla.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace _32_Hledani_cisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+            bool vstup = false;
+            Console.WriteLine("Jaké číslo mám otestovat?");
+            int hledano = int.Parse(Console.ReadLine());
+            //Ošetšení vstupu
+            while (!vstup)
+            {
+
+                if (hledano >= 0 && hledano <= 9)
+                    vstup = true;
+                else 
+                {
+                    Console.WriteLine("Zadej číslo z intervalu 0 - 9!");
+                    hledano = int.Parse(Console.ReadLine());
+                }
+            }
+
+            bool nalezen = false;
+            int cislo;
+            int pocet=0;
+
+            while (!nalezen)
+            {
+                cislo = generator.Next(0, 10);
+                pocet++;
+                Console.Write(" "+cislo+";");
+                if (cislo == hledano)
+                    nalezen = true;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Tebou hledané číslo {hledano}, bylo vygenerováno jako {pocet}.");
+        }
+    }
+}
