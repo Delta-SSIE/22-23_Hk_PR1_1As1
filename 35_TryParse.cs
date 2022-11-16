a9d9bd153d75f8d2eb642ae54e00a2f251aa9c9d
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-11-16 11:58:36 +0100

    Add files via upload

---
+++ 35_TryParse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _35_TryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej reální číslo: ");
+
+            double cislo;
+            while (!double.TryParse(Console.ReadLine(), out cislo))
+            {
+                Console.WriteLine("Má to být číslo.");
+            }
+
+            Console.WriteLine($"Zadal jsi {cislo}.");
+
+            Console.WriteLine("Zadej celé číslo: ");
+
+            int cislo2;
+            while (!int.TryParse(Console.ReadLine(), out cislo2))
+            {
+                Console.WriteLine("Má to být celé číslo.");
+            }
+            Console.WriteLine($"Zadal jsi {cislo2}.");
+        }
+    }
+}
