dac716e64e4461344badcf5e682a175fb567d8eb
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-11-25 09:14:58 +0100

    Add files via upload

---
+++ 40_ryba_for.cs
@@ -0,0 +1,29 @@
+﻿namespace _40_ryba_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kolik ryb si dáš k večeři?");
+            int pocet = int.Parse(Console.ReadLine());
+            int i = 0;
+           while (pocet>i)
+            {
+                i++;
+                Console.WriteLine("<° )))-<");
+                Console.WriteLine();
+            }
+
+
+            /*
+            Console.WriteLine("Kolik ryb si dáš k večeři?");
+            int pocet = int.Parse(Console.ReadLine());
+            for (int i = 0; i < pocet; i++)
+            {
+                Console.WriteLine("<° )))-<");
+                Console.WriteLine();
+            }*/
+            Console.ReadKey();
+        }
+    }
+}
