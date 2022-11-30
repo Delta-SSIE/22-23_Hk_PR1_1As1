27237453e1fd5b21f319b9f2f04ebe2527501a12
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-11-30 09:55:11 +0100

    Add files via upload

---
+++ 41_for_cyklus_mala_nasobilka.cs
@@ -0,0 +1,40 @@
+﻿namespace _41_for_cyklus_mala_nasobilka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Malá násobilka pomocí cyklu:");
+            for (int i = 1; i <= 10; i++)
+                Console.Write("{0} ", i);
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+                Console.Write("{0} ", i * 2);
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+                Console.Write("{0} ", i * 3);
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+                Console.Write("{0} ", i * 4);
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+                Console.Write("{0} ", i * 5);
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+                Console.Write("{0} ", i * 6);
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+                Console.Write("{0} ", i * 7);
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+                Console.Write("{0} ", i * 8);
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+                Console.Write("{0} ", i * 9);
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+                Console.Write("{0} ", i * 10);
+            Console.ReadKey();
+        }
+    }
+}
