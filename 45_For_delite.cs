16b491c1b5423c292b02137160c01df7c91f0937
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-12-02 08:24:50 +0100

    Add files via upload

---
+++ 45_For_delite.cs
@@ -0,0 +1,18 @@
+﻿namespace _45_For_delitel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vypište všechny dělitele zadaného čísla
+            Console.WriteLine("Zadej číslo, pro které chceš hledat dělitele");
+            int cislo = int.Parse(Console.ReadLine());
+
+            for (int i = cislo; i > 0; i--)
+            {
+                if (cislo % i == 0)
+                    Console.WriteLine($"{cislo} = {i} * {cislo / i}");
+            }
+        }
+    }
+}
