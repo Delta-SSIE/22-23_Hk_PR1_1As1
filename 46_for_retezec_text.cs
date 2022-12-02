a338768ed843eb7a5c9133823a6164b7b43d651e
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-12-02 08:34:41 +0100

    Add files via upload

---
+++ 46_for_retezec_text.cs
@@ -0,0 +1,24 @@
+﻿namespace _46_for_retezec_text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej větu:");
+            string veta = Console.ReadLine();
+
+            for (int i = 0; i < veta.Length; i++)
+            {
+
+                //Console.WriteLine(veta[i]);
+                Console.Write(veta[i] + " "); // vloží mezeru mezi znaky
+               /* 
+                char znak = veta[i];
+                Console.Write(znak);
+                Console.Write(" ");
+                Console.Write((int)znak); //vypíše číslo znaku v ascii https://cs.wikipedia.org/wiki/ASCII
+                Console.WriteLine();*/
+            }
+        }
+    }
+}
