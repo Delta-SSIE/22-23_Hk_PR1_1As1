cb1d198e6763834daccf1497be78b5d3d5e420e5
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-12-02 08:14:30 +0100

    Add files via upload

---
+++ 44_for_intervaly.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _44_for_intervaly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte levou mez 1. intervalu: ");
+            int levaMez1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadejte pravou mez 1. intervalu: ");
+            int pravaMez1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadejte levou mez 2. intervalu: ");
+            int levaMez2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadejte pravou mez 2. intervalu: ");
+            int pravaMez2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dvojice čísel, jejichž součet leží v některém z intervalů:");
+            Console.WriteLine("(1. číslo je z prvního intervalu a 2. z druhého intervalu)");
+            int soucet;
+            for (int i = levaMez1; i <= pravaMez1; i++)
+            {
+                for (int j = levaMez2; j <= pravaMez2; j++)
+                {
+                    soucet = i + j;
+                    if (((soucet <= pravaMez2) && (soucet >= levaMez2)) || ((soucet <= pravaMez1) && (soucet >= levaMez1)))
+                        Console.Write("[{0};{1}], ", i, j);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
