a6891afc058efa20abf8d70d638dcfdf3de47e28
horalekjos <86661098+horalekjos@users.noreply.github.com>
2022-12-09 11:05:32 +0100

    Add files via upload

---
+++ 50_Prevod_soustav_8.cs
@@ -0,0 +1,29 @@
+﻿namespace _50_Prevod_soustav_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string oktaCislo = "";
+
+            Console.Write("Zadej cislo v desítkové suustavě, které chceš převést: ");
+            int cislo = int.Parse(Console.ReadLine());
+            int cislo2 = cislo;
+            string overeni = Convert.ToString(cislo, 8);
+            Console.WriteLine("Prevedená hodnota vestavenou funkcí je: " + overeni);
+
+            while (cislo > 0)
+            {
+                int zbytek = cislo % 8;
+                cislo /= 8;
+                oktaCislo = zbytek + oktaCislo;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Prevedená hodnota {cislo2} desikové soustavy je v osmičkové soustavě: {oktaCislo}");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
