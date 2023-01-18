152c5c5b111117ea110ab2680df4c9a6c4eab478
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-01-18 13:03:37 +0100

    Add files via upload

---
+++ 58_Media.cs
@@ -0,0 +1,40 @@
+﻿namespace _58_Median
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Odchylka od mediánu
+            Console.WriteLine("Zadej počet čísel: ");
+            int pocet = int.Parse(Console.ReadLine());
+            int[] cisla = new int[pocet];
+            for (int i = 0; i < pocet; i++)
+            {
+                Console.Write("Zadej {0}. číslo: ", i + 1);
+                cisla[i] = int.Parse(Console.ReadLine());
+            }
+            // Zjednodušený medián
+            int[] cisla2 = new int[cisla.Length];
+
+            for (int i = 0; i < cisla.Length; i++)
+            {
+                cisla2[i] = cisla[i];
+            }
+            Array.Sort(cisla2);
+            Console.WriteLine("\nSeřazené pole:");
+            for (int i = 0; i < cisla2.Length; i++)
+            {
+                Console.Write(cisla2[i] + " | ");
+            }
+            float median = cisla2[cisla2.Length / 2];
+            Console.WriteLine();
+            Console.WriteLine("\nMedina je: " + median);
+            Console.ReadKey();
+            foreach (int i in cisla)
+            {
+                Console.WriteLine("\n{0} se od mediánu odchyluje o {1}", i, i - median);
+            }
+            Console.ReadKey();
+        }
+    }
+}
