308e5edb9d787f13ce0a08e57f180217b849fcd2
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-01-20 09:23:33 +0100

    Add files via upload

---
+++ 61_Palyndrom.cs
@@ -0,0 +1,23 @@
+﻿namespace _61_Palyndrom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej palindrom: ");
+            string palindrom = Console.ReadLine();
+            // Získání řetězce pozpátku
+            string pozpatku = "";
+            for (int i = palindrom.Length - 1; i >= 0; i--)
+            {
+                pozpatku += palindrom[i];
+            }
+            // Porovnání
+            if (palindrom == pozpatku)
+                Console.WriteLine("Ano, toto je palindrom.");
+            else
+                Console.WriteLine("Toto není palindrom.");
+            Console.ReadKey();
+        }
+    }
+}
