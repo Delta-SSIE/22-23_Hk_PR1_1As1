493c9feed705a9d9802d536282080af9b0bd9112
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-01-25 12:08:16 +0100

    Add files via upload

---
+++ 63_Join.cs
@@ -0,0 +1,71 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*
+            // Create string array.
+            string[] words = { "one", "two", "three" };
+
+            // Invoke the Join method.
+            // ... Print the resulting string.
+
+
+            Console.WriteLine(string.Join("|", words));
+           
+           
+
+
+
+
+
+
+           
+          string[] words = { "falcon", "wood", "cloud", "cup", "sky", "water" };
+
+          string text = string.Join(',', words, 1, 3);
+                        Console.WriteLine(text);
+
+             */
+
+            /*
+             // Creating a string array
+            // Here It contains five 
+            // elements only
+            string[] array = {"hello", " World ",
+                "Geeks", " are ", "  here " };
+
+            // Using Join method
+            // Here separator used is '/'( slash )
+            string s1 = string.Join("/", array);
+
+            // Finally after join method
+            // Getting the output of value of string s1
+            Console.WriteLine("Value of string  s1 is " + s1); 
+             */
+
+            /*
+             / Creating a string array
+        string[] array = {"lazy", "dog", "jumps", "Over",
+                                  "the", "Lazy", "fox" };
+  
+        // Using Join method
+        // Here separator used is '-'( hiphen )
+        // from index 2 and covers upto 5 
+        // elements from index 2
+        string s1 = string.Join("-", array, 2, 5);
+  
+        // Finally after joining process gets over
+        // Getting the output of value of string s1
+        Console.WriteLine("Value of string is " + s1);
+             */
+
+/*
+
+            string[] words = { "sky", "cup", "ocean", "dry", "tool", "rust" };
+
+                        string text = string.Join(",", words.Where(e => e.Length == 4));
+                        Console.WriteLine(text);*/
+        }
+    }
+}
