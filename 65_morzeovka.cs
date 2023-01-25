a0b96c3eda8c67f4c7c997b896fa09508dfe7d5a
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-01-25 12:57:00 +0100

    Add files via upload

---
+++ 65_morzeovka.cs
@@ -0,0 +1,37 @@
+﻿namespace _65_morzeovka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // řetězec, který chceme dekódovat
+            string s = ".. ... .-.. .- -. -.. ... --- ..-. -";
+            Console.WriteLine("Původní zpráva: {0}", s);
+            // řetězec s dekódovanou zprávou
+            string zprava = "";
+
+            // vzorová pole
+            string abecedniZnaky = "abcdefghijklmnopqrstuvwxyz";
+            string[] morseovyZnaky = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
+            "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+            "...-", ".--", "-..-", "-.--", "--.."};
+
+            // rozbití řetězce na znaky morzeovky
+            string[] znaky = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // iterace znaků morzeovky
+            foreach (string morseuvZnak in znaky)
+            {
+                char abecedniZnak = '?';
+                int index = Array.IndexOf(morseovyZnaky, morseuvZnak);
+                if (index >= 0) // znak nalezen
+                    abecedniZnak = abecedniZnaky[index];
+                zprava += abecedniZnak;
+            }
+
+            // výpis
+            Console.WriteLine("Dekódovaná zpráva: {0}", zprava);
+            Console.ReadKey();
+        }
+    }
+}
