610baea01f62155e6e48ab2fa5f7af79e1ab28db
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-01-25 12:34:01 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _64_Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "The quick brown,fox jumps:over the lazy dog.";
+            char[] oddelovac = { ' ', ',', ':' };
+            string[] words = phrase.Split(oddelovac);
+
+            foreach (var word in words)
+            {
+               Console.WriteLine($"<{word}>");
+            }
+        }
+    }
+}
