08a96de41ddff09ac56b412ba4abec0b8d943df7
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-01-27 08:28:37 +0100

    Add files via upload

---
+++ 67_Metoda_02.cs
@@ -0,0 +1,28 @@
+﻿namespace _67_Metoda_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = NactiCislo();
+            Console.WriteLine("Metoda načetla číslo: " + x);
+            int y = NactiCislo();
+            Console.WriteLine("Metoda načetla druhé číslo: " + y);
+            Console.ReadKey();
+        }
+
+        static int NactiCislo()
+        {
+            int cislo;
+            Console.WriteLine("Zadej číslo.");
+            while (!int.TryParse(Console.ReadLine(), out cislo))
+            {
+                Console.WriteLine("Má to být číslo.");
+            }
+           /* int cislo = int.Parse(Console.ReadLine());*/
+            return cislo;  // Vracíme hodnotu pomocá klíčového slova RETURN
+
+        }     
+            
+    }
+}
