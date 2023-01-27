eab2f298365522284f77d486a1efb3b8de04bbeb
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-01-27 08:12:27 +0100

    Add files via upload

---
+++ 66_Metoda_01.cs
@@ -0,0 +1,18 @@
+﻿namespace _66_Metoda_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pozdrav();
+            Console.ReadKey();
+        }
+
+        static void Pozdrav()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+    }
+}
