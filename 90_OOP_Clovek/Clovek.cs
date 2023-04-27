5672938d3980dec7de4303dcb5119bf0d88a5d2e
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-04-27 09:02:03 +0200

    Add files via upload

---
+++ 90_OOP_Clovek/Clovek.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lide
+{
+
+    
+    /// <summary>
+    /// Reprezentuje člověka
+    /// </summary>
+    class Clovek
+    {
+        /// <summary>
+        /// Celé jméno
+        /// </summary>
+        private string jmeno;
+        /// <summary>
+        /// Věk
+        /// </summary>
+        private int vek;
+        /// <summary>
+        /// Únava
+        /// </summary>
+        private int unava = 0;
+
+        /// <summary>
+        /// Inicializuje novou instanci
+        /// </summary>
+        /// <param name="jmeno">Celé jméno</param>
+        /// <param name="vek">Věk</param>
+        public Clovek(string jmeno, int vek)
+        {
+            this.jmeno = jmeno;
+            this.vek = vek;
+        }
+
+        /// <summary>
+        /// Spí danou dobu
+        /// </summary>
+        /// <param name="doba">Doba v hodinách</param>
+        public void Spi(int doba)
+        {
+            unava -= doba * 10;
+            Console.WriteLine("Spím a tím snížím únavu na " + unava);
+            Console.ReadKey();
+            if (unava < 0)
+                unava = 0;
+        }
+
+        /// <summary>
+        /// Uběhne danou vzdálenost
+        /// </summary>
+        /// <param name="vzdalenost">Vzdálenost v Km</param>
+        public void Behej(int vzdalenost)
+        {
+            if (unava + vzdalenost <= 20)
+            { 
+                unava += vzdalenost;
+                Console.WriteLine("Běžím");
+                Console.ReadKey();
+            }
+            else
+                Console.WriteLine("Jsem příliš unavený");
+        }
+
+        /// <summary>
+        /// Vrátí textovou reprezentaci člověka
+        /// </summary>
+        /// <returns>textová reprezentace člověka</returns>
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", jmeno, vek);
+        }
+    }
+}
---
+++ 90_OOP_Clovek/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lide
+{
+    /*
+    Naprogramujte aplikaci, která obsluhuje člověka. Člověk
+má jméno a únavu, která je zpočátku 0. Může uběhnout
+určitou vzdálenost a také spát určitou dobu. Běháním se jeho únava
+zvyšuje (1 jednotka únavy na 1 km), spaním se snižuje
+(10 jednotek únavy na 1 hodinu). Navrhněte třídu tak, aby se
+únava nikdy nemohla dostat z rozmezí 0-20 jednotek.
+Samozřejmě vám k tomu pomůže zapouzdření, únava určitě nebude
+veřejným atributem.
+
+Program vyzkoušíte tak, že necháte člověk 3x uběhnout 10
+km. Třetí uběhnutí by se nemělo povést. Když člověka necháte po
+druhém uběhnutí hodinu spát, zvládne i třetí běh.
+
+Na úvod vypište instanci člověka. Definujte mu metodu
+ToString() tak, aby se vypsalo jeho jméno a věk.
+*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clovek karel = new Clovek("Karel Nový", 25);
+            Console.WriteLine(karel);
+            karel.Behej(10);
+            karel.Behej(10);
+            karel.Spi(1);
+            karel.Behej(10);
+            Console.ReadKey();
+        }
+    }
+}
